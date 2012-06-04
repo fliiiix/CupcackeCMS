9f03942698967654bbe9ae0ad0c407e4d2ff4bce
linux4ever <justllinux-42@yahoo.com>
2012-06-04 18:20:47 +0200

    Passwort-Vergessen-Seite geaddet und einen schönen Dump aller Datenbanken gemacht

---
+++ Datenbank/cupcackecms.sql
@@ -0,0 +1,92 @@
+-- Adminer 3.3.3 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `beitrag`;
+CREATE TABLE `beitrag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titel` varchar(100) NOT NULL,
+  `untertitel` varchar(100) DEFAULT NULL,
+  `inhalt` text,
+  `id_Owner` int(11) DEFAULT NULL,
+  `Aktiv` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `bild`;
+CREATE TABLE `bild` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_beitrag` int(11) DEFAULT NULL,
+  `speicherName` varchar(50) NOT NULL,
+  `uploadName` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `cookie_mapping`;
+CREATE TABLE `cookie_mapping` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `cookie_content` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `kalenderEintrag`;
+CREATE TABLE `kalenderEintrag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datum` date NOT NULL,
+  `titel` varchar(100) NOT NULL,
+  `beschreib` longtext,
+  `id_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `pw_forgot`;
+CREATE TABLE `pw_forgot` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `link_component` varchar(128) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `rolle`;
+CREATE TABLE `rolle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `beschreib` tinytext,
+  `aktiv` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nachname` varchar(100) NOT NULL,
+  `vorname` varchar(100) NOT NULL,
+  `email` tinytext NOT NULL,
+  `pw_hash` varchar(128) NOT NULL,
+  `aktiv` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`id`, `nachname`, `vorname`, `email`, `pw_hash`, `aktiv`) VALUES
+(2,	'Mensch',	'Test',	'test@test.de',	'1ce87e773445695711406c8b2e3f7a92105dd9beb88b3908195011a20200aa53286e5a661426485588d4c9fa4e61a3198da1f7b913caa54483ddeb609da435f5',	'1');
+
+DROP TABLE IF EXISTS `user_rolle`;
+CREATE TABLE `user_rolle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `id_rolle` int(12) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2012-06-04 18:16:43
---
+++ recover_password.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!--
+	Seite, die es dem Nutzer ermöglicht ssich einen Link zum Zurücksetzen seines Passworts an seine E-Mail-Adresse senden zu lassen
+-->
+<title>CupcackeCMS - Passwort zurücksetzen</title>
+<script type="text/javascript" src="js/jquery.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+require_once('utils.php');
+if (isset($_POST["email"]) && isset($_POST["passwort_resetten"])){
+  if ($_POST["email"] == "")
+	  $errormsg = "Bitte eine E-Mail-Adresse eingeben";
+  else{
+	db_connect();
+	$ergebnis = mysql_query("SELECT id,email,vorname,nachname FROM users WHERE email=\"" . $_POST["email"] . "\"");
+	if ($row = mysql_fetch_array($ergebnis)){
+		$valid_user_id = $row["id"];
+		$valid_email = $_POST["email"];
+		$valid_name = $row["vorname"] . " " . $row["nachname"];
+	} else 
+			$errormsg = " Die E-Mail-Adresse ist nicht valide";
+	}
+	if (isset($valid_user_id)){
+		mysql_query("DELETE FROM pw_forgot WHERE user_id=" . $valid_user_id);
+		$repeat = true;
+		do{
+			$link_component = hash("haval128,3",rand(0,getrandmax()),false);
+			$ergebnis = mysql_query("SELECT * FROM pw_forgot WHERE link_component=\"" . $link_component . "\"");
+			if (mysql_num_rows($ergebnis) == 0){
+			  $repeat = false;
+			  mysql_query("INSERT INTO pw_forgot (user_id, link_component) VALUES(" . $valid_user_id . ",\"" . $link_component . "\")");
+			}
+		}while($repeat);
+		$headers = "From: noreply@fliegenberg.de" . "\n" .
+                   "X-Mailer: PHP/" . phpversion() . "\n" .
+				   "Mime-Version: 1.0" . "\n" . 
+				   "Content-Type: text/plain; charset=UTF-8" . "\n" .
+				   "Content-Transfer-Encoding: 8bit" . "\r\n";
+	    $message = "Hallo " . $valid_name . ", \r\n" .
+		           "\r\n" .
+		           "jemand hat auf fliegenberg.de ein neues Passwort für deinen Account angefordert." . "\r\n" . 
+				   "Kein Problem, hier kommt ein Link, mit dem du ein neues Passwort setzen kannst: \r\n".
+				   "\r\n".
+				   "https://fliegenberg.de/change_password.php?key=" . $link_component . "\r\n" . 
+				   "\r\n" .
+				   "Wenn du keine Änderung deines Passworts veranlasst hast, dann ignoriere diese Mail bitte einfach. \r\n".
+				   "\r\n".
+				   "Mit freundlichen Grüßen\r\n".
+				   "Dein Fliegenberg-Team";
+        mail($valid_email, "Neues Passwort für Fliegenberg.de", $message, $headers);
+	}
+  }
+}
+?>
+	</head>
+	<body>
+			<h1>CupcackeCMS - Passwort zurücksetzen/h1>
+<?php
+if (!isset($valid_user_id)){
+if (isset($errormsg)){ ?>
+<table border="1" bordercolor="#FF0000">
+  <tr>
+    <td><?php echo $errormsg; ?></td>
+  </tr>
+</table>
+<br>
+  <?php }?>
+<h1>Passwort zurücksetzen</h1>
+<b>Du hast dein Passwort vergessen? Kein Problem!</b><br>
+Gebe einfach hier deine E-Mail-Adresse mit der du dich registriert hast ein und wir schicken dir einen Link zum Zurücksetzen des Passworts für deinen Account an deine E-Mail-Adresse.<br>
+<br>
+<form name="form1" method="post" action="">
+  <table border="0">
+    <tr>
+      <td>&nbsp;E-Mail oder Username:</td>
+      <td>&nbsp;<input name="known_data" type="text" maxlength="30"></td>
+    </tr>
+    <tr>
+    <td colspan="2" align="right"><input name="passwort_resetten" type="submit" value="Passwort zurücksetzen"></td>
+    </tr>
+  </table>
+</form>
+<?php } else {?>
+Die E-Mail zum Ändern deines Passworts wurde versendet.<br>
+<a href="index.php">Zurück zur Hauptseite</a>
+<?php } ?>
+	</body>
+</html>
