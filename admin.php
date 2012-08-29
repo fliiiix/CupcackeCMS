b699d79e5d7dd0b5c942a587081b38e804049de2
linux4ever <justllinux-42@yahoo.com>
2012-08-29 22:48:20 +0200

    Ein bisschen mit dem Layout-Einbau in die Admin.php angefangen

--- admin.php
+++ admin.php
@@ -144,7 +144,55 @@ $(document).ready(function() {
   });
 });
   </script>
+  <!-- Le styles -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+  <link href="assets/css/jquery.fileupload-ui.css" rel="stylesheet">
+  <style>
+    body {
+      padding-top: 90px; /* 90px to make the container go all the way to the bottom of the topbar */
+    }
+  </style>
+
+  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Le fav and touch icons -->
+  <link rel="shortcut icon" href="/assets/ico/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/ico/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/ico/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="/assets/ico/apple-touch-icon-57-precomposed.png">
 </head><body>
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="index.php">Fliegenberg</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="kalender.php">Kalender</a></li>
+              <?php 
+                //if user login okey
+                echo "<li><a href=\"bilderGalerie.php\">Bilder Galerie</a></li>";
+              ?>
+            </ul>
+      <ul class="nav pull-right">
+              <li id="login">
+Hallo username!
+        </li>
+    </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
 <div id="create_user">
   <?php if (isset($error_msg)){
     echo "<b style=\"color:red\">" . $error_msg . "</b><br />";
