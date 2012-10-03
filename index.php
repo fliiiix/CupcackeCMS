98f3c81932e0ff9d6f5f4815136082ab74d579a2
fliiiix <fliiiix@PowerGamer.router.lan>
2012-10-03 18:03:54 +0200

    kalender ausehen position

--- index.php
+++ index.php
@@ -5,12 +5,25 @@ if (isset($login)){
 	echo "<div class=\"alert\">" . $login . " Hast du dein <a href=\"recover_password.php\">Passwort vergessen</a>?</div>";
 }
 ?>
-<p>Hier kommt irgend ein Text hin</p>
-<img alt="bildhalt" src="googleWallet.png">
-<hr />
-<h3>Nächste Events:</h3>
-<ul>
-	<li>Datum, Name, <a>link</a></li>
-	<li>Datum, Name, <a>link</a></li>
-</ul>
+<div class="span12"><h1>Titel</h1></div>
+<div class="span9">
+	<p>Hier kommt irgend ein Text hin</p>
+	<img alt="bildhalt" src="googleWallet.png">
+</div>
+<div class="span2">
+	<?php
+		require_once('utils.php');
+		db_connect();
+		if (isset($_GET['m']) && (isset($_GET['y']))) {
+			$calendar = calendar(intval($_GET['m']), intval($_GET['y']));
+		  echo $calendar['html'];
+		} else {
+			$calendar = calendar(date('m'),date('Y'));
+		  echo $calendar['html'];
+		}
+	?>
+</div>
+<div class="span12">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</div>
 <?php include 'templates/footer.tpl'; ?>
--- kalender.php
+++ kalender.php
@@ -3,12 +3,6 @@ $current_site = "Kalender";
 include 'templates/header.tpl';
 require_once('utils.php');
 db_connect();
-if (isset($_GET['m']) && (isset($_GET['y']))) {
-	$calendar = calendar(intval($_GET['m']), intval($_GET['y']));
-  echo $calendar['html'];
-} else {
-	$calendar = calendar(date('m'),date('Y'));
-  echo $calendar['html'];
-}
+
 include 'templates/footer.tpl';
 ?>
