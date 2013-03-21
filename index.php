f10b0fe3571426d0c1283e08210c53b6a4d5107e
fliiix <de-ch@hotmail.de>
2013-03-21 22:00:17 +0100

    add img

--- index.php
+++ index.php
@@ -5,23 +5,21 @@ if (isset($login)){
 	echo '<div class="alert alert-error"> <button type="button" class="close" data-dismiss="alert">×</button>' . $login . ' Hast du dein <a href="recover_password.php">Passwort vergessen</a>?</div>';
 }
 ?>
-<div class="span12"><h1>Titel</h1></div>
-<div class="span9">
-	<p>Hier kommt irgend ein Text hin</p>
+<div class="span12"><h1>Fliegenberg</h1></div>
+<div class="span8">
+	<br>
+	<img class="img-polaroid" src="http://media.fliegenberg.ch/fliegenberg.jpg">
 </div>
 <div class="span2">
 	<?php
 		require_once('utils.php');
 		if (isset($_GET['m']) && (isset($_GET['y']))) {
 			$calendar = calendar(intval($_GET['m']), intval($_GET['y']));
-		  echo $calendar;
+		    echo $calendar;
 		} else {
 			$calendar = calendar(date('m'),date('Y'));
-		  echo $calendar;
+		    echo $calendar;
 		}
 	?>
-</div>
-<div class="span12">
-	<img src="media.fliegenberg.ch/fliegenberg.jpg">
 </div>
 <?php include 'templates/footer.tpl'; ?>
